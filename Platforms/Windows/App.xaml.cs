3d7553b0843f32f069546ae0faeceaa6e9087b64
espero59601 <espero59601@gmail.com>
2026-02-25 18:43:28 -0700

    Update for local build via vlaude code

---
+++ Platforms/Windows/App.xaml.cs
@@ -0,0 +1,6 @@
+namespace ChecklistApp.WinUI;
+
+public class App : MauiWinUIApplication
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ Platforms/Windows/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+
+namespace ChecklistApp.WinUI;
+
+class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+        Application.Start((p) =>
+        {
+            var context = new DispatcherQueueSynchronizationContext(DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+            new App();
+        });
+    }
+}
